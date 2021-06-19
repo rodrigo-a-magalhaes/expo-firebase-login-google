89a6a5e4f934b169c6e970f3704fc3b27b3aa275
Rodrigo Magalhaes <Rodrigo Magalhaes>
2021-06-18 23:08:07 -0300

    add libs and pages

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+export function SignIn() {
+  return (
+    <Text>SignIn</Text>
+  )
+}
---
+++ src/routes/stack.routes.tsx
@@ -0,0 +1,34 @@
+import { createStackNavigator } from '@react-navigation/stack';
+import React from 'react';
+import { Welcome } from '../pages/Welcome';
+import { Register } from '../pages/Register';
+import { SignIn } from '../pages/SignIn';
+
+const stackRoutes = createStackNavigator();
+
+const AppRoutes: React.FC = () => (
+  <stackRoutes.Navigator
+    headerMode="none"
+    screenOptions={{
+      cardStyle: {
+        backgroundColor: '#ffffff'
+      }
+    }}
+  >
+    <stackRoutes.Screen
+      name="SignIn"
+      component={SignIn}
+    />
+    <stackRoutes.Screen
+      name="Registe"
+      component={Register}
+    />
+    <stackRoutes.Screen
+      name="Welcome"
+      component={Welcome}
+    />
+
+  </stackRoutes.Navigator>
+)
+
+export default AppRoutes;
